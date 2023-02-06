8697dd8d2943e142610d59bdbafc0ab708fa26db
Anhpham2802 <anhcherry2802@gmail.com>
2023-02-06 22:27:05 +0700

    update instruments

--- db/migrate/20230205171236_add_image_to_instruments.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToInstruments < ActiveRecord::Migration[6.0]
-  def change
-    add_column :instruments, :image, :string
-  end
-end
--- db/migrate/20230205171433_add_user_id_instruments.rb
+++
@@ -1,4 +0,0 @@
-class AddUserIdInstruments < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
